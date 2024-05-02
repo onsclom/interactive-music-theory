4381368ac0072318e7d163a6922d10ca31b0ea39
Austin Merrick <austinmerrick@onsclom.net>
2024-05-01 22:29:35 -0700

    Change red

--- src/piano.svelte
+++ src/piano.svelte
@@ -7,37 +7,37 @@
 <div class="h-28 relative overflow-x-scroll flex">
   {#each Array(octaves) as _, i}
     <div
-      class="hover:bg-red-500 cursor-pointer border-2 absolute h-full"
+      class="hover:bg-red-300 cursor-pointer border-2 absolute h-full"
       style:left={`${i * noteSize * 7}px`}
       style:width={`${noteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer border-2 absolute h-full"
+      class="hover:bg-red-300 cursor-pointer border-2 absolute h-full"
       style:left={`${noteSize + i * noteSize * 7}px`}
       style:width={`${noteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer border-2 absolute h-full"
+      class="hover:bg-red-300 cursor-pointer border-2 absolute h-full"
       style:left={`${noteSize * 2 + i * noteSize * 7}px`}
       style:width={`${noteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer border-2 absolute h-full"
+      class="hover:bg-red-300 cursor-pointer border-2 absolute h-full"
       style:left={`${noteSize * 3 + i * noteSize * 7}px`}
       style:width={`${noteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer border-2 absolute h-full"
+      class="hover:bg-red-300 cursor-pointer border-2 absolute h-full"
       style:left={`${noteSize * 4 + i * noteSize * 7}px`}
       style:width={`${noteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer border-2 absolute h-full"
+      class="hover:bg-red-300 cursor-pointer border-2 absolute h-full"
       style:left={`${noteSize * 5 + i * noteSize * 7}px`}
       style:width={`${noteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer border-2 absolute h-full"
+      class="hover:bg-red-300 cursor-pointer border-2 absolute h-full"
       style:left={`${noteSize * 6 + i * noteSize * 7}px`}
       style:width={`${noteSize}px`}
     ></div>
@@ -45,27 +45,27 @@
 
     <!-- black keys -->
     <div
-      class="hover:bg-red-500 cursor-pointer h-16 absolute bg-black"
+      class="hover:bg-red-300 cursor-pointer h-16 absolute bg-black"
       style:left={`${noteSize / 2 + (noteSize - blackNoteSize) / 2 + i * noteSize * 7}px`}
       style:width={`${blackNoteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer h-16 absolute bg-black"
+      class="hover:bg-red-300 cursor-pointer h-16 absolute bg-black"
       style:left={`${noteSize / 2 + (noteSize - blackNoteSize) / 2 + noteSize + i * noteSize * 7}px`}
       style:width={`${blackNoteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer h-16 absolute bg-black"
+      class="hover:bg-red-300 cursor-pointer h-16 absolute bg-black"
       style:left={`${noteSize / 2 + (noteSize - blackNoteSize) / 2 + noteSize * 3 + i * noteSize * 7}px`}
       style:width={`${blackNoteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer h-16 absolute bg-black"
+      class="hover:bg-red-300 cursor-pointer h-16 absolute bg-black"
       style:left={`${noteSize / 2 + (noteSize - blackNoteSize) / 2 + noteSize * 4 + i * noteSize * 7}px`}
       style:width={`${blackNoteSize}px`}
     ></div>
     <div
-      class="hover:bg-red-500 cursor-pointer h-16 absolute bg-black"
+      class="hover:bg-red-300 cursor-pointer h-16 absolute bg-black"
       style:left={`${noteSize / 2 + (noteSize - blackNoteSize) / 2 + noteSize * 5 + i * noteSize * 7}px`}
       style:width={`${blackNoteSize}px`}
     ></div>
